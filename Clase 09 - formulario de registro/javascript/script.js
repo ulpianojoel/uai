d5e0e4b5fb1b95e7a58f82b6f4c0675aee26b180
ulpianojoel <130317542+ulpianojoel@users.noreply.github.com>
2023-06-04 16:38:02 -0300

    cambio del script
    cambie luego de ver que funciona el js a su carpeta

--- Clase 09 - formulario de registro/javascript/script.js
+++ Clase 09 - formulario de registro/javascript/script.js
@@ -1,20 +1,27 @@
-// Ejemplo de JavaScript inicial para deshabilitar el envío de formularios si hay campos no válidos
-(function () {
-  'use strict'
+document.addEventListener("DOMContentLoaded", function() {
+  var form = document.getElementById("myForm");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita el envío automático del formulario
 
-  // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
-  var forms = document.querySelectorAll('.needs-validation')
+    // Realiza las validaciones personalizadas
+    var name = document.getElementById("validationDefault01").value;
+    var email = document.getElementById("validationDefaultUsername").value;
+    var ciudad = document.getElementById("validationDefault03").value;
 
-  // Bucle sobre ellos y evitar el envío
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
+    if (name === "" || email === "" || ciudad === "") {
+      alert("Por favor, completa todos los campos obligatorios.");
+    } else if (!validateEmail(email)) {
+      alert("Por favor, ingresa un correo electrónico válido.");
+    } else {
+      // Si todas las validaciones pasan, puedes enviar el formulario
+      alert("El formulario se envió correctamente.");
+      // aca van las acciones post envio
+    }
+  });
 
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
+  function validateEmail(email) {
+    // Utiliza una expresión regular para validar el formato del correo electrónico
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+});
